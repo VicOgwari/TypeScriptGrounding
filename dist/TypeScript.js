577c53384098e4fb8397d67c9eb5c4e598e5628a
VicOgwari <vicogwari@gmail.com>
2024-08-09 04:29:15 -0700

    Typescript grounding

---
+++ dist/TypeScript.js
@@ -0,0 +1,12 @@
+"use strict";
+let age = 20;
+if (age < 50)
+    age += 10;
+console.log(age);
+let numbers = ["1", "2", "3"];
+numbers.forEach(n => console.log(n));
+let assorts = [22, "The quick brown fox!"];
+;
+let mySize = 2;
+console.log(mySize);
+//# sourceMappingURL=TypeScript.js.map
---
+++ src/TypeScript.ts
@@ -0,0 +1,52 @@
+let age: number = 20;
+if (age < 50)
+    age += 10;
+console.log(age);
+
+//Arrays
+let numbers = ["1", "2", "3"];
+numbers.forEach(n => console.log(n));
+
+//Tuples
+let assorts: [number, string] = [22, "The quick brown fox!"];
+
+//Enum using PascalCase
+const enum Size {Small = 1, Medium, Large};
+let mySize = Size.Medium;
+console.log(mySize);
+
+
+//Functions
+function calculateTax(income: number, taxYear = 2022): number{
+    if(taxYear == 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+
+calculateTax(50_000);
+
+//Objects using type alias
+type Employee = {
+    readonly id: number,
+     name: string,
+     retire: (date: Date) => void
+
+}
+let employee: Employee = {id: 1, name: "Vic", retire: (date: Date) => { console.log(date);}};
+
+//Union types
+function kgToLbs(weight: number | string): number{
+    if(typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+
+
+//Intersection types
+type Dragable = {drag: () => void};
+type Resizable = {resize: () => void};
+type UIWidget = Dragable & Resizable;
+let textBox: UIWidget = {
+    drag: () => {}, resize: () => {}
+}
